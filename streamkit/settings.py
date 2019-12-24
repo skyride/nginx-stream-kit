a1809f1eaadfaac63b215a05b27fbbe91dfc476c
A Findlay <skylinerspeeder@gmail.com>
2019-12-24 00:46:24 +0000

    config tweaks

--- streamkit/settings.py
+++ streamkit/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = '/media/'
