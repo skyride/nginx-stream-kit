0a0a2f4cf9b3389035b61733936fc92fa4bec9aa
A Findlay <skylinerspeeder@gmail.com>
2020-01-17 21:26:34 +0000

    added storages

--- streamkit/settings.py
+++ streamkit/settings.py
@@ -96,6 +96,17 @@ CACHES = {
 }
 
 
+# Media Files
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_ENDPOINT_URL = os.environ['S3_ENDPOINT_URL']
+AWS_S3_REGION_NAME = os.environ['S3_REGION_NAME']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['S3_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET_KEY']
+AWS_AUTO_CREATE_BUCKET = False
+AWS_DEFAULT_ACL = 'public-read'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
