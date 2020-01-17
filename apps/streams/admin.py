ac686c070ec0622c48f98c7c7348390966d3c0b8
A Findlay <skylinerspeeder@gmail.com>
2020-01-17 22:57:32 +0000

    Added distribution/segment models

--- apps/streams/admin.py
+++ apps/streams/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Stream
+from .models import Stream, Distribution, Segment
 
 
 @admin.register(Stream)
@@ -17,4 +17,24 @@ class StreamAdmin(admin.ModelAdmin):
     list_filter = (
         "app",
         "status"
+    )
+
+
+@admin.register(Distribution)
+class DistributionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "stream",
+        "name",
+        "created"
+    )
+
+
+@admin.register(Segment)
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "distribution",
+        "sequence_number",
+        "created"
     )
--- apps/streams/models.py
+++ apps/streams/models.py
@@ -37,3 +37,47 @@ class Stream(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class Distribution(models.Model):
+    """
+    A distribution is an encoded version of a stream. E.g. 480p, 240p,
+    or source.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    stream = models.ForeignKey(Stream,
+        related_name="distributions",
+        on_delete=models.CASCADE)
+    name = models.CharField(max_length=64, db_index=True)
+
+    created = models.DateTimeField(db_index=True, auto_now_add=True)
+    last_updated = models.DateTimeField(db_index=True, auto_now=True)
+
+    class Meta:
+        unique_together = ("stream", "name")
+
+    def __str__(self):
+        return self.name
+
+
+def generate_segment_filename(instance, filename):
+    return f"{instance.distribution_id}/{uuid4()}.ts"
+
+class Segment(models.Model):
+    """
+    A segment is a single video file as part of a distribution.
+    """
+    distribution = models.ForeignKey(Distribution,
+        related_name="segments",
+        on_delete=models.CASCADE)
+    sequence_number = models.IntegerField()
+    file = models.FileField(upload_to=generate_segment_filename)
+
+    created = models.DateTimeField(db_index=True, auto_now_add=True)
+    last_updated = models.DateTimeField(db_index=True, auto_now=True)
+
+    class Meta:
+        unique_together = ("distribution", "sequence_number")
+
+    def __str__(self):
+        return str(self.file)
