e67a986fe5d46ac0ddda8714d87190a7f9564511
A Findlay <skylinerspeeder@gmail.com>
2020-01-18 02:45:27 +0000

    playlist generation

--- apps/watch/urls.py
+++ apps/watch/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 
 urlpatterns = [
+    path("playlists/distributions/<uuid:distribution_id>.m3u8",
+        views.DistributionPlaylistView.as_view())
 ]
--- apps/watch/views.py
+++ apps/watch/views.py
@@ -1,2 +1,26 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.views import View
+
+from apps.streams.models import Distribution, Segment
+
+
+class DistributionPlaylistView(View):
+    template = "watch/playlists/distribution.m3u8"
+
+    def get(self, request, distribution_id):
+        """
+        Return a live m3u8 file for this distribution.
+        """
+        distribution: Distribution = get_object_or_404(Distribution,
+            id=distribution_id,
+            stream__status="live")
+
+        segments = distribution.segments.order_by('-sequence_number')[:6]
+        segments = list(reversed(segments))
+        context = {'segments': segments}
+
+        return HttpResponse(
+            content=render(request, self.template, context),
+            content_type="application/x-mpegURL"
+            )
